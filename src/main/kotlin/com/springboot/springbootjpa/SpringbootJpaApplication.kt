0f26310ad68c64d94226bd4aa897fa0ea25b195f
임채성 <pan1129@naver.com>
2022-11-04 21:51:27 +0900

    springboot-jpa commit

---
+++ src/main/kotlin/com/springboot/springbootjpa/SpringbootJpaApplication.kt
@@ -0,0 +1,11 @@
+package com.springboot.springbootjpa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootJpaApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringbootJpaApplication>(*args)
+}
---
+++ src/test/kotlin/com/springboot/springbootjpa/SpringbootJpaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.springboot.springbootjpa
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootJpaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
