3d8ba0109bfc2ba9e56b1a330a079c71d9da2e0f
임채성 <pan1129@naver.com>
2022-11-04 23:05:44 +0900

    Book Entity DB 설계 구현

---
+++ src/main/kotlin/com/springboot/back/entity/Book.kt
@@ -0,0 +1,28 @@
+package com.springboot.back.entity
+
+import lombok.Getter
+import lombok.Setter
+import lombok.ToString
+import java.time.LocalDateTime
+import javax.persistence.*
+
+@Entity
+@Table(name = "books")
+@Getter @Setter
+@ToString
+data class Book (
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    val id: Long ?= null,
+
+    val title: String,
+
+    val book_name: String,
+
+    val content: String,
+
+    val createdDate : LocalDateTime
+
+    ){
+
+}
